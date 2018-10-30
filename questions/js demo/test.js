c934a1f4820c0397af531750f25783761773aafb
firefly_may <m544498510@gmail.com>
2018-10-30 22:12:59 +0800

    test ssh

--- questions/js demo/test.js
+++ questions/js demo/test.js
@@ -4,6 +4,6 @@ function Person(){};
 var p = new Person();
 console.log('p.a: '+ p.a); // p.a: undefined
 console.log('p.b: '+ p.b); // p.b: b
-console.log('P.a', Person.a)
+console.log('P.a', Person.a);
 
 
