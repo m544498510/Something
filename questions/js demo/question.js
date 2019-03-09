a795ade4fb836af68bd65c69813b44ec17313dd7
firefly_may <m544498510@gmail.com>
2019-03-09 21:17:55 +0800

    add new question

--- questions/js demo/question.js
+++ questions/js demo/question.js
@@ -35,6 +35,29 @@ function simpleToLocaleString2(num){
 /**
  * 2. 实现 Promise.some([]) 方法，有一个 resolve ，就resolve，全部 reject，才 reject
  */
+Promise.some = function (promiseArr){
+  const rejectErrorArr = [];
+  return new Promise(((resolve, reject) => {
+    promiseArr.forEach(promise => {
+      promise
+        .then(data => resolve(data))
+        .catch(err => {
+          rejectErrorArr.push(err);
+          if(rejectErrorArr.length === promiseArr.length){
+            reject(rejectErrorArr);
+          }
+        })
+    })
+  }))
+};
+/*
+const p = new Promise(resolve => setTimeout(() => resolve('settimeout'), 0));
+Promise.some([p, Promise.resolve('1'), Promise.resolve('2')])
+  .then(data => console.log('resolve', data))
+  .catch(e => console.log('reject', e));
+*/
+
+
 
 /**
  * 3. 
@@ -46,4 +69,102 @@ function serializationSymbol(){
   };
   console.log(JSON.stringify(tmp));
 }
-serializationSymbol();
+//serializationSymbol();
+
+/***
+ * 4. 给定一个字符串，找出其中无重复字符的最长子字符串长度
+ */
+function getNoRepeatWordSubstr(str){
+  let substr = '';
+  let tmpArr = [];
+  str.split('').forEach(word => {
+    if(tmpArr.includes(word)){
+      substr = compare(substr, tmpArr);
+      const repeatIndex = tmpArr.indexOf(word);
+      tmpArr.splice(0, repeatIndex + 1);
+    }
+    tmpArr.push(word);
+  });
+  substr = compare(substr, tmpArr);
+  return substr;
+  
+  function compare(str, arr){
+    if(str.length < arr.length){
+      str = arr.join('');
+    }
+    return str;
+  }
+}
+
+//console.log(getNoRepeatWordSubstr("qweqasderty"));
+
+/***
+ * 5. 实现超出整数存储范围的两个大正整数相加
+ */
+
+function bigIntAddtion(num1, num2){
+  const maxLen = 4;
+  const num1Tmp = parseBigInt(num1);
+  const num2Tmp = parseBigInt(num2);
+  const result = [];
+  let needAdd1 = false;
+  for (let i = 0; i < num1Tmp.length || i < num2Tmp.length; i++) {
+    const tmp1 = parseInt(num1Tmp[i]) || 0;
+    const tmp2 = parseInt(num2Tmp[i]) || 0;
+    
+    let resultTmp =  tmp1 + tmp2;
+    if(needAdd1){
+      resultTmp += 1;
+    }
+    resultTmp = String(resultTmp);
+    
+    if(resultTmp.length > maxLen){
+      result.unshift(resultTmp.slice(1, resultTmp.length));
+      needAdd1 = true;
+    }else{
+      result.unshift(resultTmp);
+      needAdd1 = false;
+    }
+  }
+  return result.join("");
+  
+  function parseBigInt(str){
+    const result = [];
+    const len = str.length;
+    for (let i = len - 1; i >= 0; i-= maxLen ) {
+      let start = i - maxLen + 1;
+      start = start >= 0 ? start : 0; 
+      result.push(str.slice(start, i + 1));
+    }
+    return result;
+  }
+}
+//console.log(bigIntAddtion('1115111', '11115111'));
+
+/***
+ *  6. 将数组扁平化并去除其中重复数据，最终得到一个升序且不重复的数组
+ */
+function flatArray(arr) {
+  return [...(new Set(arr.flat(Infinity)))].sort((a, b) => a - b);
+}
+//console.log(flatArray([[3,2,1],[3,1,2],[9,7,[7,4],[[5,[6,8]]]]]));
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- questions/react.md
+++ questions/react.md
@@ -1,11 +1,34 @@
 1. react 虚拟DOM 是什么? 如何实现? 说一下diff算法 ?
+
 2. react和vue的比较 ? 
+
 3. DIFF算法为什么是O(n)复杂度而不是O(n^3)
+
 4. react 生命周期， v16 和 之前的区别，新加的方法有什么用，有用过吗？
+
 5. 为什么要删掉 componentWillReceiveProps，之前用的很顺手啊？
+
 6. react HOC 原理，作用，什么情况下会选择用 HOC
+
 7. react-router 中 withRouter 实现原理
+
 8. Redux-saga 对比 redux-thunk 和 redux-promise 的优势，为什么要引一个这么大的包
+
+9. 写 React/Vue 项目时为什么要在组件中写 key，其作用是什么？
+
+   在执行diff算法时，更快的找到对应的节点
+
+10. **angular 双向数据绑定与vue数据的双向数据绑定**
+
+    1. 二者都是 MVVM 模式开发的典型代表
+    2. angular 是通过脏检测实现，angular 会将 UI 事件，请求事件，settimeout 这类延迟，的对象放入到事件监测的脏队列，当数据变化的时候，触发 $diget 方法进行数据的更新，视图的渲染
+    3. vue 通过数据属性的数据劫持和发布订阅的模式实现，大致可以理解成由3个模块组成，observer 完成对数据的劫持，compile 完成对模板片段的渲染，watcher 作为桥梁连接二者，订阅数据变化及更新视图
+
+11. React 可以用两种方法声明 Component，它们有什么区别？什么情况你会选择哪一种？
+
+    扩展问题：如果是 functional Component，为什么我们还要在第一行引入 import React from 'react'? 在这个文件中，应该根本就没有用到 React 的库。
+
+     
 
  
 
